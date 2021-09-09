129c1039d7e087f3a96dc8e28668902164b15527
beomki-yeo <beomki.yeo@gmail.com>
2021-09-09 16:22:51 -0700

    backup

---
+++ tests/cuda/grid/grid_tests.cpp
@@ -0,0 +1,24 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#include <gtest/gtest.h>
+
+// This defines the local frame test suite
+TEST(algorithms, grid) {
+
+
+
+}
+
+// Google Test can be run manually from the main() function
+// or, it can be linked to the gtest_main library for an already
+// set-up main() function primed to accept Google Test test cases.
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
