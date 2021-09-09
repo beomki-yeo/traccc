c02881c9320b12a318a9cc51395f4769ce7190cc
beomki-yeo <beomki.yeo@gmail.com>
2021-09-09 12:46:56 -0700

    add detray build

---
+++ cmake/traccc-detray.cmake
@@ -0,0 +1,25 @@
+# TRACCC library, part of the ACTS project (R&D line)
+#
+# (c) 2021 CERN for the benefit of the ACTS project
+#
+# Mozilla Public License Version 2.0
+
+# Guard against multiple includes.
+include_guard( GLOBAL )
+
+# Tell the user what's happening.
+message( STATUS "Building detray as part of the traccc project" )
+
+# Declare where to get VecMem from.
+FetchContent_Declare( Detray
+   GIT_REPOSITORY "https://github.com/acts-project/detray.git" )
+
+# Prevent Detray from building its tests and benchmarks
+# builds/uses GoogleTest.
+set( DETRAY_UNIT_TESTS OFF )
+set( DETRAY_BENCHMARKS OFF )
+
+# Get it into the current directory.
+FetchContent_Populate( Detray )
+add_subdirectory( "${detray_SOURCE_DIR}" "${detray_BINARY_DIR}"
+  EXCLUDE_FROM_ALL )
