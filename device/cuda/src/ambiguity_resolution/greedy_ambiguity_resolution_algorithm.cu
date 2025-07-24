3cced1f6471e17d6b35e9b08d97905ad501a2ddf
Beomki Yeo <beomki.yeo@gmail.com>
2025-07-24 11:54:40 -0700

    Add n_valid_threads variable

--- device/cuda/src/ambiguity_resolution/greedy_ambiguity_resolution_algorithm.cu
+++ device/cuda/src/ambiguity_resolution/greedy_ambiguity_resolution_algorithm.cu
@@ -302,7 +302,6 @@ greedy_ambiguity_resolution_algorithm::operator()(
         if (unique_meas_counts[i] == 1) {
             continue;
         } else if (unique_meas_counts[i] > 1) {
-            
 
         } else {
             throw std::runtime_error(
@@ -390,6 +389,8 @@ greedy_ambiguity_resolution_algorithm::operator()(
         vecmem::make_unique_alloc<unsigned int>(m_mr.main);
     vecmem::unique_alloc_ptr<unsigned int> n_meas_to_remove_device =
         vecmem::make_unique_alloc<unsigned int>(m_mr.main);
+    vecmem::unique_alloc_ptr<unsigned int> n_valid_threads_device =
+        vecmem::make_unique_alloc<unsigned int>(m_mr.main);
 
     // Device objects
     int is_first_iteration = 1;
@@ -474,7 +475,8 @@ greedy_ambiguity_resolution_algorithm::operator()(
                 .n_removable_tracks = n_removable_tracks_device.get(),
                 .n_meas_to_remove = n_meas_to_remove_device.get(),
                 .meas_to_remove_view = meas_to_remove_buffer,
-                .threads_view = threads_buffer});
+                .threads_view = threads_buffer,
+                .n_valid_threads = n_valid_threads_device.get()});
 
         kernels::remove_tracks<<<1, 1024, 0, stream>>>(
             device::remove_tracks_payload{
@@ -491,13 +493,13 @@ greedy_ambiguity_resolution_algorithm::operator()(
                 .n_shared_view = n_shared_buffer,
                 .rel_shared_view = rel_shared_buffer,
                 .n_removable_tracks = n_removable_tracks_device.get(),
-                .n_meas_to_remove = n_meas_to_remove_device.get(),
                 .meas_to_remove_view = meas_to_remove_buffer,
                 .threads_view = threads_buffer,
                 .terminate = terminate_device.get(),
                 .n_updated_tracks = n_updated_tracks_device.get(),
                 .updated_tracks_view = updated_tracks_buffer,
-                .is_updated_view = is_updated_buffer});
+                .is_updated_view = is_updated_buffer,
+                .n_valid_threads = n_valid_threads_device.get()});
 
         // The seven kernels below are to keep sorted_ids sorted based on
         // the relative shared measurements and pvalues. This can be reduced
--- device/cuda/src/ambiguity_resolution/kernels/remove_tracks.cu
+++ device/cuda/src/ambiguity_resolution/kernels/remove_tracks.cu
@@ -73,7 +73,7 @@ __global__ void remove_tracks(device::remove_tracks_payload payload) {
         (*payload.n_accepted) -= *(payload.n_removable_tracks);
     }
 
-    if (threadIndex < *(payload.n_meas_to_remove)) {
+    if (threadIndex < *(payload.n_valid_threads)) {
         sh_meas_ids[threadIndex] = meas_to_remove[threadIndex];
         sh_threads[threadIndex] = threads[threadIndex];
         is_valid_thread = true;
@@ -115,7 +115,7 @@ __global__ void remove_tracks(device::remove_tracks_payload payload) {
         track_status[worst_idx] = 0;
 
         int n_sharing_tracks = 1;
-        for (unsigned int i = threadIndex + 1; i < *(payload.n_meas_to_remove);
+        for (unsigned int i = threadIndex + 1; i < *(payload.n_valid_threads);
              ++i) {
 
             if (sh_meas_ids[i] == id && sh_threads[i] != sh_threads[i - 1]) {
