59fbd824b0670ad0f2d9f66a5b0b9b6c18fa6c5b
Beomki Yeo <beomki.yeo@gmail.com>
2025-08-01 21:16:52 -0700

    Backup

--- device/cuda/src/ambiguity_resolution/greedy_ambiguity_resolution_algorithm.cu
+++ device/cuda/src/ambiguity_resolution/greedy_ambiguity_resolution_algorithm.cu
@@ -299,7 +299,8 @@ greedy_ambiguity_resolution_algorithm::operator()(
 
     // Fill tracks per measurement vector
     {
-        const unsigned int nThreads = m_warp_size * 2;
+        //const unsigned int nThreads = m_warp_size * 2;
+        const unsigned int nThreads = 1024;
         const unsigned int nBlocks = meas_count;
 
         kernels::sort_tracks_per_measurement<<<nBlocks, nThreads, 0, stream>>>(
--- device/cuda/src/ambiguity_resolution/kernels/remove_tracks.cu
+++ device/cuda/src/ambiguity_resolution/kernels/remove_tracks.cu
@@ -69,6 +69,17 @@ __launch_bounds__(512) __global__
 
     const unsigned n_accepted_prev = *(payload.n_accepted);
 
+    /*
+    if (threadIdx.x == 0) {
+        for (int i = 0; i < tracks_per_measurement.size(); i++) {
+            for (int j = 0; j < tracks_per_measurement[i].size(); j++) {
+                printf("%d ", tracks_per_measurement[j]);
+            }
+            printf("\n");
+        }
+    }
+    */
+
     __syncthreads();
 
     if (threadIndex == 0) {
@@ -106,7 +117,8 @@ __launch_bounds__(512) __global__
             sorted_ids.at(n_accepted_prev - 1 - sh_threads[threadIndex]);
 
         unsigned int worst_idx =
-            thrust::find(thrust::seq, tracks.begin(), tracks.end(), trk_id) -
+            thrust::lower_bound(thrust::seq, tracks.begin(), tracks.end(),
+                                trk_id) -
             tracks.begin();
 
         track_status[worst_idx] = 0;
@@ -120,8 +132,8 @@ __launch_bounds__(512) __global__
 
                 trk_id = sorted_ids[n_accepted_prev - 1 - sh_threads[i]];
 
-                worst_idx = thrust::find(thrust::seq, tracks.begin(),
-                                         tracks.end(), trk_id) -
+                worst_idx = thrust::lower_bound(thrust::seq, tracks.begin(),
+                                                tracks.end(), trk_id) -
                             tracks.begin();
 
                 track_status[worst_idx] = 0;
